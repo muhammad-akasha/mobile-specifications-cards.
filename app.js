68e2aaf737820bfa93504a7708d5e01de9749ded
Muhammad Akasha <153507162+muhammad-akasha@users.noreply.github.com>
2024-06-01 16:40:39 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,433 @@
+let mobileObj = {
+    Apple: {
+        models: {
+            "iphone 11": {
+                src: "./images/iphone11.jpg",
+                screenSize: "6.1 inches",
+                frontCamera: "12 mega pixels",
+                backCamera: "dual 12 mega pixels",
+                colors: "Black, Green, Yellow, Purple, Red, White",
+                fingerPrint: "NO",
+                ram: "4gb",
+                storage: ["64gb", "128gb", "256gb"],
+                battery: "3110 mah",
+                ios: "13 upgradable to 17",
+                price: "85499 PKR"
+
+            },
+            "iphone 12 pro": {
+                src: "./images/iphone12pro.jpg",
+                screenSize: "6.7 inches",
+                frontCamera: "12 mega pixels",
+                backCamera: "triple 12 mega pixels",
+                colors: "Black, Green, Yellow, Purple, Red, White",
+                fingerPrint: "NO",
+                ram: "6gb",
+                storage: ["64gb", "128gb", "256gb", "512gb"],
+                battery: "3687 mah",
+                ios: "14.1 upgradable to 17.5.1",
+                price: "120999 PKR"
+
+            },
+            "iphone 15 pro": {
+                src: "./images/iphone15pro.jpg",
+                screenSize: "6.7 inches",
+                frontCamera: "32 mega pixels",
+                backCamera: "triple 48 , 12 and 12 mega pixels",
+                colors: "Black, Green, Yellow, Purple, Red, White",
+                fingerPrint: "NO",
+                ram: "8gb",
+                storage: ["128gb", "256gb", "512gb"],
+                battery: "4441 mah",
+                ios: "14.1 upgradable to 17.6.1",
+                price: "210999 PKR"
+
+            }
+        }
+    },
+    Samsung: {
+        models: {
+            "Galaxy A55": {
+                src: "./images/galaxya55.jpg",
+                screenSize: "6.6 inches",
+                frontCamera: "32 mega pixels",
+                backCamera: "tripel 50 , 12 and 5 mega pixels",
+                colors: "Black, Green, Yellow, Purple, Red, White",
+                fingerPrint: "Yes",
+                ram: "4gb",
+                storage: ["64gb", "128gb", "256gb"],
+                battery: "5000 mah",
+                Android: "Android 14",
+                price: "77299 PKR"
+
+            },
+            "Galaxy S24 Ultra": {
+                src: "./images/galaxys24ultra.jpg",
+                screenSize: "6.8 inches",
+                frontCamera: "32 mega pixels",
+                backCamera: "Quad 200 , 10 , 50 and 12 mega pixels",
+                colors: "Titanium Black, Titanium Gray, Titanium Violet , Titanium Green, Titanium Orange",
+                fingerPrint: "Yes",
+                ram: "12gb",
+                storage: ["256gb", "1TB"],
+                battery: "5000 mah",
+                Android: "Android 14",
+                price: "282999 PKR"
+
+            },
+            "Galaxy F55": {
+                src: "./images/galaxyf55.jpg",
+                screenSize: "6.7 inches",
+                frontCamera: "28 mega pixels",
+                backCamera: "tripel 32 , 12 and 5 mega pixels",
+                colors: "Black, Green, Purple, Red, White",
+                fingerPrint: "Yes",
+                ram: "6gb",
+                storage: ["64gb", "128gb", "256gb"],
+                battery: "4800 mah",
+                Android: "Android 11",
+                price: "55299 PKR"
+            },
+        }
+    },
+    Techno: {
+        models: {
+            "Spark 5": {
+                src: "./images/spark5.jpg",
+                screenSize: "6.2 inches",
+                frontCamera: "28 mega pixels",
+                backCamera: "dual 12 and 5 mega pixels",
+                colors: "Black, Green, gray , White",
+                fingerPrint: "Yes",
+                ram: "3gb , 4gb",
+                storage: ["32gb", "64gb", "128gb"],
+                battery: "4800 mah",
+                Android: "Android 12",
+                price: "33299 PKR"
+
+            },
+            "Pova 2": {
+                src: "./images/pova2.jpg",
+                screenSize: "6.6 inches",
+                frontCamera: "32 mega pixels",
+                backCamera: "dual 20 and 12 mega pixels",
+                colors: "black , blue , purple , white , gray",
+                fingerPrint: "Yes",
+                ram: "4gb , 6gb",
+                storage: ["32gb", "64gb", "128gb"],
+                battery: "5000 mah",
+                Android: "Android 14",
+                price: "45999 PKR"
+
+            },
+            "Spark 10": {
+                src: "./images/spark10.jpg",
+                screenSize: "6.0 inches",
+                frontCamera: "15 mega pixels",
+                backCamera: "20 mega pixels",
+                colors: "Black, Green, Purple, Red, White",
+                fingerPrint: "Yes",
+                ram: "4gb ,  6gb , 12gb ",
+                storage: ["4gb", "64gb", "128gb"],
+                battery: "4800 mah",
+                Android: "Android 10",
+                price: "31299 PKR"
+            },
+        }
+    },
+    Vivo: {
+        models: {
+            "Vivo Y11": {
+                src: "./images/y11.jpg",
+                screenSize: "6.2 inches",
+                frontCamera: "28 mega pixels",
+                backCamera: "dual 10 and 5 mega pixels",
+                colors: "Black, Blue, Gray , White",
+                fingerPrint: "Yes",
+                ram: "3gb , 4gb",
+                storage: ["32gb", "64gb", "128gb"],
+                battery: "5000 mah",
+                Android: "Android 9",
+                price: "20299 PKR"
+
+            },
+            "Vivo S1": {
+                src: "./images/s1.jpg",
+                screenSize: "6.6 inches",
+                frontCamera: "28 mega pixels",
+                backCamera: "dual 20 and 12 mega pixels",
+                colors: "black , blue , purple , white , gray",
+                fingerPrint: "Yes",
+                ram: "4gb , 6gb",
+                storage: ["32gb", "64gb", "128gb"],
+                battery: "5000 mah",
+                Android: "Android 14",
+                price: "45999 PKR"
+
+            },
+            "Vivo Y19": {
+                src: "./images/y19.jpg",
+                screenSize: "6.75 inches",
+                frontCamera: "22 mega pixels",
+                backCamera: "tripel 20 , 12 and 5 mega pixels",
+                colors: "Black, Green, Purple, Red, White",
+                fingerPrint: "Yes",
+                ram: "4gb , 6gb ",
+                storage: ["4gb", "64gb", "128gb"],
+                battery: "6000 mah",
+                Android: "Android 11",
+                price: "55299 PKR"
+            },
+        }
+    },
+    Oppo: {
+        models: {
+            "A18": {
+                src: "./images/a18.jpg",
+                screenSize: "6.2 inches",
+                frontCamera: "28 mega pixels",
+                backCamera: "dual 12 and 5 mega pixels",
+                colors: "Black, Green, gray , White",
+                fingerPrint: "Yes",
+                ram: "3gb , 4gb",
+                storage: ["32gb", "64gb", "128gb"],
+                battery: "4800 mah",
+                Android: "Android 12",
+                price: "33299 PKR"
+
+            },
+            "Reno 12": {
+                src: "./images/reno12.jpg",
+                screenSize: "6.6 inches",
+                frontCamera: "22 mega pixels",
+                backCamera: "dual 23 and 12 mega pixels",
+                colors: "black , purple , white , gray",
+                fingerPrint: "Yes",
+                ram: "4gb , 6gb",
+                storage: ["32gb", "64gb", "128gb"],
+                battery: "5000 mah",
+                Android: "Android 13",
+                price: "45999 PKR"
+
+            },
+            "F9 Pro": {
+                src: "./images/f9pro.jpg",
+                screenSize: "6.75 inches",
+                frontCamera: "25 mega pixels",
+                backCamera: "tripel 30 , 12 and 5 mega pixels",
+                colors: "Black, Green, Purple, White",
+                fingerPrint: "Yes",
+                ram: "6gb , 12gb ",
+                storage: ["64gb", "128gb", "256gb"],
+                battery: "4000 mah",
+                Android: "Android 11",
+                price: "55299 PKR"
+            },
+        }
+    },
+    Realme: {
+        models: {
+            "Note 5 Pro": {
+                src: "./images/note5pro.jpg",
+                screenSize: "6.2 inches",
+                frontCamera: "21 mega pixels",
+                backCamera: "dual 20 and 5 mega pixels",
+                colors: "Black, Green, gray , White",
+                fingerPrint: "Yes",
+                ram: "3gb , 4gb",
+                storage: ["32gb", "64gb", "128gb"],
+                battery: "4800 mah",
+                Android: "Android 12",
+                price: "33299 PKR"
+
+            },
+            "Note 10": {
+                src: "./images/note10.jpg",
+                screenSize: "6.6 inches",
+                frontCamera: "40 mega pixels",
+                backCamera: "dual 25 and 12 mega pixels",
+                colors: "black , blue , purple , white , gray",
+                fingerPrint: "Yes",
+                ram: "4gb , 6gb",
+                storage: ["32gb", "64gb", "128gb"],
+                battery: "5000 mah",
+                Android: "Android 14",
+                price: "45999 PKR"
+
+            },
+            "13 C": {
+                src: "./images/13c.jpg",
+                screenSize: "5.80 inches",
+                frontCamera: "25 mega pixels",
+                backCamera: "32 mega pixels",
+                colors: "Black, Green, Purple, Red, White",
+                fingerPrint: "Yes",
+                ram: "4gb , 6gb ",
+                storage: ["32", "64gb"],
+                battery: "3000 mah",
+                Android: "Android 9",
+                price: "35299 PKR"
+            },
+        }
+    },
+    Motorola: {
+        models: {
+            "Moto Z Slim": {
+                src: "./images/motozslim.jpg",
+                screenSize: "6.0 inches",
+                frontCamera: "21 mega pixels",
+                backCamera: "24 mega pixels",
+                colors: "Black, gray , White",
+                fingerPrint: "Yes",
+                ram: "3gb , 4gb",
+                storage: ["32gb", "64gb", "128gb"],
+                battery: "2800 mah",
+                Android: "Android 9",
+                price: "20299 PKR"
+
+            },
+            "Moto G Power": {
+                src: "./images/motogpower.jpg",
+                screenSize: "6.6 inches",
+                frontCamera: "30 mega pixels",
+                backCamera: "25 mega pixels",
+                colors: "black , blue , purple , white , gray",
+                fingerPrint: "Yes",
+                ram: "4gb , 6gb",
+                storage: ["32gb", "64gb", "128gb"],
+                battery: "5000 mah",
+                Android: "Android 12",
+                price: "45599 PKR"
+
+            },
+            "Moto Z force": {
+                src: "./images/motozforce.jpg",
+                screenSize: "6.25 inches",
+                frontCamera: "20 mega pixels",
+                backCamera: "32 mega pixels",
+                colors: "Black , gray, White",
+                fingerPrint: "Yes",
+                ram: "4gb , 6gb ",
+                storage: ["32gb", "64gb", "128gb"],
+                battery: "3700 mah",
+                Android: "Android 10",
+                price: "31299 PKR"
+            },
+        }
+    },
+    Sony: {
+        models: {
+            "Xperia 1": {
+                src: "./images/xperia1.jpg",
+                screenSize: "5.5 inches",
+                frontCamera: "14 mega pixels",
+                backCamera: "dual 10 and 5 mega pixels",
+                colors: "Black, Green, gray , White",
+                fingerPrint: "No",
+                ram: "3gb , 4gb",
+                storage: ["32gb", "64gb", "128gb"],
+                battery: "3600 mah",
+                Android: "Android 8",
+                price: "28299 PKR"
+
+            },
+            "Xperia 5": {
+                src: "./images/xperia5.jpg",
+                screenSize: "6.0 inches",
+                frontCamera: "20 mega pixels",
+                backCamera: "dual 25 and 12 mega pixels",
+                colors: "black , blue , purple , white , gray",
+                fingerPrint: "Yes",
+                ram: "4gb , 6gb",
+                storage: ["32gb", "64gb", "128gb"],
+                battery: "4000 mah",
+                "android/ios": "Android 11",
+                price: "55999 PKR"
+
+            },
+            "Xperia 10": {
+                src: "./images/xperia10.jpg",
+                screenSize: "6.72 inches",
+                frontCamera: "25 mega pixels",
+                backCamera: "tripel 24 , 12 and 5 mega pixels",
+                colors: "Black, Green, Purple, Red, White",
+                fingerPrint: "Yes",
+                ram: "6gb , 12gb ",
+                storage: ["64gb", "128gb", "256gb"],
+                battery: "6000 mah",
+                android: "Android 11",
+                price: "65299 PKR"
+            },
+        }
+    },
+}
+
+
+let mobileBrand = document.getElementById("mobile");
+let allModels = document.getElementById("allmodels");
+let resultDiv = document.getElementById("result");
+let button = document.getElementById("search");
+let brand, models, selectedModel;
+console.log(mobileBrand);
+mobileBrand.innerHTML = ` <option value="">--- Select Brand ---</option>`
+
+for (let mobile in mobileObj) {
+    // console.log(mobile);
+    let option = document.createElement("option");
+    option.style.textTransform = "capitalize"
+    option.text = mobile;
+    option.value = mobile;
+    mobileBrand.appendChild(option);
+}
+
+mobileBrand.addEventListener("change", function(e) {
+    brand = e.target.value;
+    // console.log(brand);
+    models = mobileObj[brand]['models'];
+    // console.log(models);
+    allModels.innerHTML = ` <option value="">--- Select Model ---</option>`
+    for (let model in models) {
+        // console.log(model);
+        let option = document.createElement("option");
+        option.text = model;
+        option.value = model;
+        allModels.appendChild(option);
+    }
+});
+allModels.addEventListener("change", function(e) {
+    selectedModel = e.target.value;
+    console.log(selectedModel);
+    console.log(brand, models, selectedModel)
+
+});
+
+
+function showResult() {
+    if (brand && selectedModel) {
+        let mobileSpec = mobileObj[brand].models[selectedModel]
+        let result = `
+     <div class="result-div">
+    <img src="${mobileSpec['src']}">
+    <h2> Screen Size  : <strong> ${mobileSpec['screenSize']} </strong> </h2>
+    <h2> Front Camera  : <strong> ${mobileSpec['frontCamera']}</strong></h2>
+    <h2> Back Camera  : <strong> ${mobileSpec['backCamera']}</strong></h2>
+    <h2> Colors : <strong> ${mobileSpec['colors'].split(",").join("")}</strong></h2>`
+        if (mobileSpec.hasOwnProperty("Android")) {
+            result += `<h2> Android   :   <strong> ${mobileSpec['Android']}</strong> </h2>`
+        } else {
+            result += `<h2>  IOS : <strong> ${mobileSpec['ios']}</strong> </h2>`
+        }
+        result += `
+    <h2> FingerPrint Sensor : <strong> ${mobileSpec['fingerPrint']}</strong></h2>
+    <h2> Ram : <strong> ${mobileSpec['ram'].split(",").join("")}</strong></h2>
+    <h2> Storage : <strong> ${mobileSpec['storage'].join(" ")}</strong></h2>
+    <h2> Battery : <strong> ${mobileSpec['battery']}</strong></h2>
+    <h2> Price : <strong> ${mobileSpec['price']}</strong></h2>
+    </div>
+    `
+        resultDiv.innerHTML = result;
+        button.disabled = false;
+    }
+}
+}
+}
